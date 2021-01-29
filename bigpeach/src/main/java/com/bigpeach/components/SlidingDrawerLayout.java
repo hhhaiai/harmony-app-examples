f5d452b05b8a6f067796053cecd40a587fafde23
liukerui <lkr_lkr@163.com>
2021-01-29 18:28:36 +0800

    优化上滑抽屉控件

--- bigpeach/src/main/java/com/bigpeach/components/SlidingDrawerLayout.java
+++ bigpeach/src/main/java/com/bigpeach/components/SlidingDrawerLayout.java
@@ -14,12 +14,13 @@ import ohos.hiviewdfx.HiLogLabel;
 
 public class SlidingDrawerLayout extends StackLayout {
 
-    HiLogLabel label = new HiLogLabel(HiLog.LOG_APP, 0x00011, "big");
+    HiLogLabel label = new HiLogLabel(HiLog.LOG_APP, 0x00012, "big");
 
     private Component child;
     private int windowHeight;
     private int offsetHeight;
     private int offsetAuto;
+    private int middleY;
     private int minY;
     private int maxY;
     private float startY;
@@ -29,6 +30,7 @@ public class SlidingDrawerLayout extends StackLayout {
     private float y = 0;
     private boolean intercept = false;
     private ShapeElement shapeElement;
+    private boolean opened = false;
 
     public SlidingDrawerLayout(Context context) {
         this(context, null);
@@ -54,32 +56,38 @@ public class SlidingDrawerLayout extends StackLayout {
                 Point point = dragInfo.downPoint;
                 int pX = point.getPointXToInt();
                 int pY = point.getPointYToInt();
-                HiLog.error(label, "onDragDown1 dx=%{public}s dy=%{public}s", point.getPointXToInt(), point.getPointYToInt());
+                //HiLog.error(label, "onDragDown1 dx=%{public}s dy=%{public}s", point.getPointXToInt(), point.getPointYToInt());
 
                 Rect rect = child.getComponentPosition();
-                HiLog.error(label, "onDragDown2 left=%{public}s right=%{public}s top=%{public}s bottom=%{public}s", rect.left, rect.right, rect.top, rect.bottom);
+                //HiLog.error(label, "onDragDown2 left=%{public}s right=%{public}s top=%{public}s bottom=%{public}s", rect.left, rect.right, rect.top, rect.bottom);
 
                 float[] floats = child.getContentPosition();
                 x = floats[0];
                 y = floats[1];
-                HiLog.error(label, "onDragDown3 x=%{public}s y=%{public}s", floats[0], floats[1]);
+                //HiLog.error(label, "onDragDown3 x=%{public}s y=%{public}s", floats[0], floats[1]);
 
                 intercept = x < pX && pX < x + (rect.right - rect.left) && y < pY && pY < y + (rect.bottom - rect.top);
                 if (intercept && animator != null) animator.stop();
+                // 动画过程中点击动画停止，松开继续恢复到最近边界
+                if (y > middleY) {
+                    startY = maxY;
+                } else {
+                    startY = minY;
+                }
             }
 
             @Override
             public void onDragStart(Component component, DragInfo dragInfo) {
-                Point point = dragInfo.startPoint;
-                HiLog.error(label, "onDragStart y=%{public}s yO=%{public}s", point.getPointYToInt(), dragInfo.yOffset);
-                y = y + (float) dragInfo.yOffset;
-                startY = y;
+                //Point point = dragInfo.startPoint;
+                //HiLog.error(label, "onDragStart y=%{public}s yO=%{public}s", point.getPointYToInt(), dragInfo.yOffset);
+
+                y = y + (float) dragInfo.yOffset;   //  dragInfo.yOffset 永远等于0;
                 child.setContentPosition(x, y);
             }
 
             @Override
             public void onDragUpdate(Component component, DragInfo dragInfo) {
-                Point point = dragInfo.updatePoint;
+                //Point point = dragInfo.updatePoint;
                 //HiLog.error(label, "onDragUpdate y=%{public}s yO=%{public}s yV=%{public}s", point.getPointYToInt(), (int) dragInfo.yOffset, (int) dragInfo.yVelocity);
                 y = y + (float) dragInfo.yOffset;
                 if (y > maxY) {
@@ -88,7 +96,7 @@ public class SlidingDrawerLayout extends StackLayout {
                     y = minY;
                 }
                 child.setContentPosition(x, y);
-                float alpha = (float) (0xCC * Math.pow((maxY - y) / offsetHeight, 5));
+                float alpha = (float) (0xA6 * Math.pow((maxY - y) / offsetHeight, 5));
                 //HiLog.error(label, "alpha=%{public}s", alpha);
                 shapeElement.setAlpha((int) alpha);
             }
@@ -96,22 +104,33 @@ public class SlidingDrawerLayout extends StackLayout {
             @Override
             public void onDragEnd(Component component, DragInfo dragInfo) {
                 float offsetY = y - startY;
-                HiLog.error(label, "onDragEnd offsetY=%{public}s offsetAuto=%{public}s", offsetY, offsetAuto);
+                //HiLog.error(label, "onDragEnd offsetY=%{public}s offsetAuto=%{public}s", offsetY, offsetAuto);
                 if (offsetY > 0 && offsetY > offsetAuto) {          // 向下滑动
-                    if (y < maxY) startAnimator(child, y, maxY);
+                    if (y < maxY) {
+                        startAnimator(false, y, maxY);
+                    } else {
+                        opened = false;
+                    }
                 } else if (offsetY < 0 && -offsetY > offsetAuto) {  // 向上滑动
-                    if (y > minY) startAnimator(child, y, minY);
-                } else {
-                    startAnimator(child, y, startY);
+                    if (y > minY) {
+                        startAnimator(true, y, minY);
+                    } else {
+                        opened = true;
+                    }
+                } else {                                            // 恢复动画
+                    startAnimator(opened, y, startY);
                 }
                 intercept = false;
             }
 
             @Override
             public void onDragCancel(Component component, DragInfo dragInfo) {
-                Point point = dragInfo.updatePoint;
-                HiLog.error(label, "onDragCancel y=%{public}s", point.getPointYToInt());
-                intercept = false;
+                //Point point = dragInfo.updatePoint;
+                //HiLog.error(label, "onDragCancel y=%{public}s", point.getPointYToInt());
+                if(intercept){
+                    startAnimator(opened, y, startY);
+                    intercept = false;
+                }
             }
 
             @Override
@@ -121,25 +140,31 @@ public class SlidingDrawerLayout extends StackLayout {
         });
     }
 
-    public void setChild(Component component, int handHeight, int bodyHeight) {
+    public void setDrawer(Component component, int handleHeight, int contentHeight) {
         this.child = component;
-        this.offsetHeight = bodyHeight;
-        this.minY = windowHeight - handHeight - bodyHeight;
-        this.maxY = windowHeight - handHeight;
-        this.offsetAuto = bodyHeight / 3;
+        this.child.setClickable(true);
+        this.offsetHeight = contentHeight;
+        this.minY = windowHeight - handleHeight - contentHeight;
+        this.maxY = windowHeight - handleHeight;
+        this.offsetAuto = contentHeight / 3;
+        this.middleY = windowHeight - (handleHeight + contentHeight) / 2;
     }
 
-    public void showMenu() {
+    public boolean isOpened() {
+        return opened;
+    }
+
+    public void open() {
         float y = child.getContentPositionY();
-        startAnimator(child, y, minY);
+        startAnimator(true, y, minY);
     }
 
-    public void closeMenu() {
+    public void close() {
         float y = child.getContentPositionY();
-        startAnimator(child, y, maxY);
+        startAnimator(false, y, maxY);
     }
 
-    private void startAnimator(Component component, float startValue, float endValue) {
+    private void startAnimator(boolean open, float startValue, float endValue) {
         if (animator != null) animator.stop();
         float value = endValue - startValue;
         animator = new AnimatorValue();
@@ -148,13 +173,14 @@ public class SlidingDrawerLayout extends StackLayout {
         animator.setStateChangedListener(new JStateChangedListener() {
             @Override
             public void onStop(Animator anim) {
+                opened = open;
                 animator = null;
             }
         });
         animator.setValueUpdateListener((animatorValue, v) -> {
             y = startValue + (value * v);
             child.setContentPosition(0, y);
-            float alpha = (float) (0xCC * Math.pow((maxY - y) / offsetHeight, 5));
+            float alpha = (float) (0xA6 * Math.pow((maxY - y) / offsetHeight, 5));
             //HiLog.error(label, "alpha=%{public}s", alpha);
             shapeElement.setAlpha((int) alpha);
         });
--- entry/src/main/java/com/example/harmonyapp/slice/SlidingDrawerAbilitySlice.java
+++ entry/src/main/java/com/example/harmonyapp/slice/SlidingDrawerAbilitySlice.java
@@ -1,26 +1,132 @@
 package com.example.harmonyapp.slice;
 
 import com.bigpeach.ability.JAbilitySlice;
+import com.bigpeach.components.ListItemProvider;
 import com.bigpeach.components.SlidingDrawerLayout;
+import com.bigpeach.components.viewholder.AbilityHolder;
+import com.bigpeach.components.viewholder.ItemViewHolder;
+import com.bigpeach.components.viewholder.datamodel.ItemDataModel;
 import com.example.harmonyapp.ResourceTable;
 import ohos.aafwk.content.Intent;
+import ohos.agp.colors.RgbColor;
+import ohos.agp.components.Button;
 import ohos.agp.components.Component;
+import ohos.agp.components.ListContainer;
+import ohos.agp.components.Text;
+import ohos.agp.components.element.ShapeElement;
+import ohos.agp.utils.Color;
+import ohos.agp.utils.LayoutAlignment;
+import ohos.agp.window.dialog.ToastDialog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SlidingDrawerAbilitySlice extends JAbilitySlice {
 
 
-    SlidingDrawerLayout slidingDrawer;
+    private ListContainer listContainer;
+    private SlidingDrawerLayout slidingDrawer;
 
     @Override
     protected void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_slice_sliding_drawer);
 
-        slidingDrawer = findComponent(ResourceTable.Id_sliding_drawer);
-        Component drawerLayout = findComponent(ResourceTable.Id_drawer);
-        drawerLayout.setMarginBottom(vp2px(-500f)); // 鸿蒙系统目前 ohos:bottom_margin="-500vp" 单位vp与px值相等的Bug
-        slidingDrawer.setChild(drawerLayout, vp2px(80f), vp2px(500f));
-        Component handleLayout = findComponent(ResourceTable.Id_handle);
-        Component contentLayout = findComponent(ResourceTable.Id_content);
+        {
+            listContainer = findComponent(ResourceTable.Id_list_container);
+            List<TextInfo> data = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                data.add(new TextInfo("" + (i + 1)));
+            }
+            listContainer.setFooterBoundarySwitch(false);
+            listContainer.setHeaderBoundarySwitch(false);
+            listContainer.setBoundarySwitch(true);
+            listContainer.setBoundaryColor(Color.WHITE);
+            listContainer.setBoundaryThickness(vp2px(8f));
+            ListItemProvider<TextInfo> itemProvider = new ListItemProvider<>();
+            itemProvider.setDataList(data);
+            listContainer.setItemProvider(itemProvider);
+        }
+        {
+            slidingDrawer = findComponent(ResourceTable.Id_sliding_drawer);
+            Component drawerLayout = findComponent(ResourceTable.Id_drawer);
+            drawerLayout.setMarginBottom(vp2px(-500f)); // 鸿蒙系统目前 ohos:bottom_margin="-500vp" 单位vp与px值相等的Bug
+            slidingDrawer.setDrawer(drawerLayout, vp2px(60f), vp2px(500f));
+            Component lineComponent = findComponent(ResourceTable.Id_line);
+            ShapeElement shapeElement = new ShapeElement();
+            shapeElement.setRgbColor(RgbColor.fromArgbInt(0xFFFFFFFF));
+            shapeElement.setCornerRadius(10f);
+            lineComponent.setBackground(shapeElement);
+            Component contentLayout = findComponent(ResourceTable.Id_content);
+
+            Button button = findComponent(ResourceTable.Id_button);
+            ShapeElement shapeElement1 = new ShapeElement();
+            shapeElement1.setRgbColor(RgbColor.fromArgbInt(0xFF00587a));
+            shapeElement1.setCornerRadius(vp2px(60f));
+            button.setBackground(shapeElement1);
+            button.setClickedListener(component -> {
+                if (slidingDrawer.isOpened()) {
+                    slidingDrawer.close();
+                } else {
+                    slidingDrawer.open();
+                }
+
+            });
+        }
+    }
+
+    /**
+     * 数据
+     */
+    public static class TextInfo extends ItemDataModel {
+
+        private String text;
+
+        public TextInfo(String text) {
+            this.text = text;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        @Override
+        public int getLayoutId(int index) {
+            return ResourceTable.Layout_item_text;
+        }
+
+        @Override
+        public ItemViewHolder getViewHolder(AbilityHolder iAbility, ListContainer listContainer, ListItemProvider itemProvider, Component component) {
+            return new StringHolder(iAbility, listContainer, itemProvider, component);
+        }
+    }
+
+    /**
+     * view
+     */
+    public static class StringHolder extends ItemViewHolder<AbilityHolder, TextInfo> {
+
+        Text text;
+
+        public StringHolder(AbilityHolder iAbility, ListContainer listContainer, ListItemProvider itemProvider, Component itemComponent) {
+            super(iAbility, listContainer, itemProvider, itemComponent);
+            text = findComponent(ResourceTable.Id_text);
+            text.setMarginsLeftAndRight(vp2px(3f), vp2px(3f));
+            ShapeElement shapeElement = new ShapeElement();
+            shapeElement.setCornerRadius(10);
+            shapeElement.setRgbColor(RgbColor.fromArgbInt(0xFFd7385e));
+            text.setBackground(shapeElement);
+            itemComponent.setClickedListener(component -> {
+                new ToastDialog(getContext())
+                        .setText("点我干啥？")
+                        .setAlignment(LayoutAlignment.CENTER)
+                        .show();
+            });
+        }
+
+        @Override
+        public void setContent(int index, TextInfo data) {
+            text.setText(data.getText());
+        }
     }
 }
